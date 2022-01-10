4120c8664ddb5084478a11bba51020c1eb576e7b
Kashuu-kaba <Kabashu12@yahoo.ne.jp>
2022-01-10 12:17:47 +0000

    1/10 commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,7 +6,7 @@ class TodolistsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to "/homes/top"
+      redirect_to todolists_path, notice: "List was created successufly!"
     else
       render :new
     end
@@ -20,6 +20,18 @@ class TodolistsController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+    @lists = List.all
+    if @lists.destroy_all
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # get 'todolists/show'
   # get 'todolists/edit'
   root to: 'homes#top'
+  delete 'todolists/destroy_all'
   resources 'todolists'
 end
